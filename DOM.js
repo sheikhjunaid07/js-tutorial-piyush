792fc02363647122723628f1fec1e02bfa6f7202
sheikhjunaid07 <sheikhjunaid732000@gmail.com>
2024-10-29 21:36:45 +0530

    about querySelector

--- DOM.js
+++ DOM.js
@@ -8,5 +8,18 @@ console.log(window);
 
 console.log("junaid");
 
-//right way is
+//right way is but it is not compulsory to write window every time
 window.console.log("Junaid");
+
+//document object- you can see your all html code using document object
+console.log(document);
+
+//you can change the thing also
+document.title = "DOM";
+//if you see your title is changed now
+
+//you can also access the body of the html file
+console.log(document.body);
+
+//with document.write method you can write on the browser or the web page
+document.write("This sentence write using (document.write)");
